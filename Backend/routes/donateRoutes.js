7d5b07e4f981915513822a85c618d77f34a9dae0
farhanaislam30 <168390472+farhanaislam30@users.noreply.github.com>
2025-03-09 05:55:57 +0600

    Update donateRoutes

--- Backend/routes/donateRoutes.js
+++ Backend/routes/donateRoutes.js
@@ -1,7 +1,19 @@
-import express from "express";
-import { addDonateItem } from "../controllers/donateController.js";
+import { addFood } from "../controllers/foodController.js";
 import multer from "multer";
+import express from "express";
 
-const donateRouter = express.Router();
-donateRouter.post("/add", addDonateItem);
-export default donateRouter;
+const foodRouter = express.Router();
+
+// Image Storage Engine
+const storage = multer.diskStorage({
+  destination: "uploads",
+  filename: (req, file, cb) => {
+    return cb(null, `${Date.now()}${file.originalname}`);
+  },
+});
+
+const upload = multer({ storage: storage });
+
+foodRouter.post("/add", upload.single("image"), addFood);
+
+export default foodRouter;
