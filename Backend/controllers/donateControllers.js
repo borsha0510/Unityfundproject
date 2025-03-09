d0dcb6a3dedd798d7b24a976e8c56ee7f0d3a893
farhanaislam30 <168390472+farhanaislam30@users.noreply.github.com>
2025-03-09 06:08:46 +0600

    edit donateControllers

--- Backend/controllers/donateControllers.js
+++ Backend/controllers/donateControllers.js
@@ -1,6 +1,46 @@
 import donateModels from "../models/DonateModels";
 import fs from "fs";
 // add  donate item
-const addDonateItem = async (req, res) => {};
 
-export default { addDonateItem };
+export const addDonateItem = async (req, res) => {
+  let image_filename = `${req.file.filename}`;
+
+  const donate = new donateModels({
+    name: req.body.name,
+    description: req.body.description,
+    price: req.body.price,
+    category: req.body.category,
+    image: image_filename,
+  });
+
+  try {
+    await donate.save();
+    res.json({ success: true, message: "Item Added" });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: "Error" });
+  }
+};
+
+const listDonate = async (req, res) => {
+  try {
+    const foods = await donateModels.find({});
+    res.json({ success: true, data: foods });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: "Error" });
+  }
+};
+
+const removeDonate = async (req, res) => {
+  try {
+    const foods = await donateModels.findById(req.body.id);
+    fs.unlink("uploads/${food.image},()=>{}");
+    await donateModels.findByIdAndDelete(req.body.id);
+    res.json({ success: true, message: "Donate Removed" });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: "Error" });
+  }
+};
+export default { addDonateItem, listDonate, removeDonate };
--- Backend/index.js
+++ Backend/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import connectDB from "./config/db.js";
+import donateRouter from "./routes/donateRoutes.js";
 //app config
 const app = express();
 const PORT = 4000;
@@ -12,11 +13,13 @@ app.use(cors());
 connectDB();
 
 //api endpoints
-app.use("/api/donate", require("./routes/donateRoutes"));
+app.use("/api/donate", donateRouter);
+app.use("images", express.static("uploads"));
 
 app.get("/", (req, res) => {
-  res.send("  API  working");
+  res.send("API Working");
 });
+
 app.listen(PORT, () => {
   console.log("Server Started on http://localhost: ${PORT}");
 });
